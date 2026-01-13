f5665bff6233aa7cd236243e92eb51800d0ea79b
pavelkovvv <pavelkovvv@yandex.ru>
2026-01-13 19:47:15 +0300

    feat: Добавлен базовый каркас main.py файла

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="PlayPulse",
+    version="0.0.1",
+)
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
