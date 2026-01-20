b582ebb524ded205b52a71d409da206531bb65ef
pavelkovvv <pavelkovvv@yandex.ru>
2026-01-20 16:07:48 +0300

    feat: Добавлена конфигурация для всинхронной работы SQLAlchemy с БД

--- src/database.py
+++ src/database.py
@@ -1,5 +1,31 @@
-from sqlalchemy.orm import DeclarativeBase
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.pool import NullPool
 
+from settings import config_loader
+
+
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+asyncpg://{config_loader('POSTGRES_DB_USER')}:"
+    f"{config_loader('POSTGRES_DB_PASSWORD')}@"
+    f"{config_loader('POSTGRES_DB_HOST')}:"
+    f"{config_loader('POSTGRES_DB_PORT')}/"
+    f"{config_loader('POSTGRES_DB_NAME')}"
+)
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URI, poolclass=NullPool)
+
+SessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 class Base(DeclarativeBase):
     pass
+
+
+async def get_obj_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
