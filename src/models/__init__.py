00023e0bbe07a3bc814abaea05097fb1af73a28f
pavelkovvv <pavelkovvv@yandex.ru>
2026-03-09 12:08:23 +0300

    fix: Обновление .gitignore
    Также изменен __init__.py файл (удален лишний импорт)

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,4 +3,3 @@ from src.models.game import Game
 from src.models.user_game_status import UserGameStatus
 from src.models.rating import Rating
 from src.models.review import Review
-from src.models.user_game_status import UserGameStatus
